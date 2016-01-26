c7e44f2ab2793547958c60ae2c101c0a67cb91fe
Gregory Kohler <gregorykohler@gmail.com>
2016-01-25 16:34:16 -0800

    correct fonts destination in gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -54,7 +54,7 @@ bundler.on('log', $.util.log);
 gulp.task('fonts', function() {
   return gulp.src(require('main-bower-files')({
     filter: '**/*.{eot,svg,ttf,woff,woff2}'
-  }).concat('app/fonts/**/*')).pipe(gulp.dest('dist/fonts'));
+  }).concat('app/fonts/**/*')).pipe(gulp.dest('distribution/fonts'));
 });
 
 
